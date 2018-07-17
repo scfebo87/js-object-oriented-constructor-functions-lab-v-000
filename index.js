66792d93f4501b94a279722457f01d1813d2b6d7
Sean Christopher Febo <scf@techie.com>
2018-07-17 22:31:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ function Scooter(year, color, model) {
   this.color = color;
   this.model = model;
 }
+
+function Driver(name, age, experience) {
+  this.year = year;
+  this.color = color;
+  this.model = model;
+}
+
+function Scooter(year, color, model) {
+  this.year = year;
+  this.color = color;
+  this.model = model;
+}
